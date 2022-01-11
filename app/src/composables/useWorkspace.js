9d5a085f681d84e9603cb0f9d1da10d031060a9b
0xMMelvin <mmelvin0581@gmail.com>
2022-01-10 19:58:53 -0500

    prep fe for deployment

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,8 +2,9 @@ import { inject, provide, computed } from 'vue'
 import { useAnchorWallet } from '@solana/wallet-adapter-vue'
 import { Program, Provider } from '@project-serum/anchor'
 import { Connection, PublicKey } from '@solana/web3.js'
-import idl from '../../../target/idl/solana_twitter.json'
+import idl from '@/idl/solana_twitter.json'
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 const preflightCommitment = 'processed'
 const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
@@ -13,7 +14,7 @@ export const useWorkspace = () => inject(workspaceSymbol)
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet()
-  const connection = new Connection('http://127.0.0.1:8899', commitment)
+  const connection = new Connection(clusterUrl, commitment)
   const provider = computed(
     () =>
       new Provider(connection, wallet.value, {
